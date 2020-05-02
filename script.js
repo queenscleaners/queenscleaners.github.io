1fa4bc027a61d942b93aa474d82aa4b42ea45ba8
qctoronto <strykez3r0@gmail.com>
2020-05-02 02:46:40 -0400

    2 spaces gang

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const slideshow_timer = 5000;   // in millseconds
-const slideshow_max = 5;        // # of images
+const slideshow_timer = 5000; // in millseconds
+const slideshow_max = 5; // # of images
 var slideshow_ind = 0;
 
 var backtotop_div = document.getElementById("backtotop-text");
 backtotop_div.addEventListener("click", () => {
-    $("html").animate( { 
-        scrollTop: 0 
-    }, 500);
+  $("html").animate({
+    scrollTop: 0
+  }, 500);
 });
 
 var timeouts = [];
@@ -21,25 +21,24 @@ leftarrow.addEventListener("click", () => navigateImage(-1));
 rightarrow.addEventListener("click", () => navigateImage(1));
 
 function changeImage(dir, repeat) {
-    if (dir > 0) {
-        slideshow_ind++ 
-    }
-    else {
-        slideshow_ind--;
-        if (slideshow_ind < 0)
-            slideshow_ind = slideshow_max - 1;
-    }
-    slideshow_ind %= 5;
-    imagegallery_img.src = "images/image" + slideshow_ind + ".jpg";
-    imagegallery_text.textContent = (slideshow_ind + 1) + " / " + slideshow_max;
-    // clear timeouts
-    for (let i = 0; i < timeouts.length; i++) {
-        clearTimeout(timeouts[i]);
-    }
-    // new timeout
-    timeouts.push(setTimeout(() => changeImage(1, true), slideshow_timer));
+  if (dir > 0) {
+    slideshow_ind++
+  } else {
+    slideshow_ind--;
+    if (slideshow_ind < 0)
+      slideshow_ind = slideshow_max - 1;
+  }
+  slideshow_ind %= 5;
+  imagegallery_img.src = "images/image" + slideshow_ind + ".jpg";
+  imagegallery_text.textContent = (slideshow_ind + 1) + " / " + slideshow_max;
+  // clear timeouts
+  for (let i = 0; i < timeouts.length; i++) {
+    clearTimeout(timeouts[i]);
+  }
+  // new timeout
+  timeouts.push(setTimeout(() => changeImage(1, true), slideshow_timer));
 }
 
 function navigateImage(dir) {
-    changeImage(dir, false);
+  changeImage(dir, false);
 }
